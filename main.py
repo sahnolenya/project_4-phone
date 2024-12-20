405c011d76e3e2521eb975546a11bc952318f190
sahnolenya <lyonya.saxno@mail.ru>
2024-12-20 14:24:13 +0300

    File paths

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 import pandas as pd
+
+input_file = "C:\\Users\\sakhn\\OneDrive\\Рабочий стол\\phone_numbers.xlsx"
+
+output_file = "C:\\Users\\sakhn\\OneDrive\\Рабочий стол\\cleaned_phone_numbers.xlsx"
